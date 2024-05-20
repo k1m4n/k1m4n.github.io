ec8cf6bd4cd22c86bf33a7dac0e6b500f0d9b57a
k1m4n <164094858+k1m4n@users.noreply.github.com>
2024-05-20 01:27:11 -0500

    Add files via upload

---
+++ finalproject/output.js
@@ -0,0 +1,158 @@
+ [
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tAlabama\t8727122.89155926000000000\t13363463.84318800000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tAlaska\t54686979.30088730000000000\t150737803.62171900000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tArizona\t1075280.39365703000000000\t29535713.06780170000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tArkansas\t7153896.34648884000000000\t13769059.03424870000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tCalifornia\t10576452.182986000000000000\t40961694.037056600000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tColorado\t4038547.347255790000000000\t26974269.375832000000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tConnecticut\t973101.504210357000000000\t1289227.68691963000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tDelaware\t168870.510472792000000000\t529624.390238424000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tDistrict of Columbia\t2966.59359596225000000\t16607.29425652150000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tFlorida\t7198022.64588843000000000\t14745992.97963560000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tGeorgia\t9330400.24668051000000000\t15243415.41539280000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tHawaii\t627237.05985490100000000\t1672428.79068314000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tIdaho\t6394972.53562264000000000\t21643984.14543670000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tIllinois\t2070409.060314910000000000\t15008781.128761300000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tIndiana\t2011124.743691150000000000\t9436269.46739418000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tIowa\t1034406.54901444000000000\t14584483.20372350000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tKansas\t702581.391589892000000000\t21312412.64239800000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tKentucky\t5229070.39828058000000000\t10452628.17057460000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tLouisiana\t6322359.64909597000000000\t12073541.71081540000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tMaine\t7232808.96070149000000000\t8436322.72907095000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tMaryland\t1188196.067042610000000000\t2566845.724826310000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tMassachusetts\t1522509.224708350000000000\t2102299.97492696000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tMichigan\t8491942.769668690000000000\t25036039.459514700000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tMinnesota\t7935952.838049960000000000\t22534484.303086700000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tMississippi\t7504960.34578008000000000\t12354580.85865930000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tMissouri\t6238288.599289990000000000\t18056895.598580100000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tMontana\t6451666.826406220000000000\t38089679.179280600000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tNebraska\t210881.05718386400000000\t20020894.60454030000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tNevada\t366183.18571840700000000\t28628390.77606820000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tNew Hampshire\t2059383.287109980000000000\t2399771.827138420000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tNew Jersey\t1100623.657778730000000000\t1969753.272853050000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tNew Mexico\t1413026.94411860000000000\t31486424.76669230000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tNew York\t8539325.263339620000000000\t13518667.789825900000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tNorth Carolina\t7650492.12292166000000000\t12781834.60544610000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tNorth Dakota\t533189.84168115500000000\t18314183.692269100000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tOhio\t3448635.394718010000000000\t11993914.468001200000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tOklahoma\t2852167.92368884000000000\t18104042.978476200000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tOregon\t9713338.759431900000000000\t25141108.862172600000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tPennsylvania\t7366618.719391970000000000\t11729832.583865600000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tRhode Island\t185281.06956090800000000\t283337.95335067000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tSouth Carolina\t4994998.98664837000000000\t8012426.78541978000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tSouth Dakota\t425616.69547081200000000\t19974628.97710290000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tTennessee\t5901462.374030740000000000\t10918203.95846800000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tTexas\t7295023.59625730000000000\t68621326.86624390000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tUtah\t1562489.210138390000000000\t21983247.848674900000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tVermont\t1989056.944645080000000000\t2494847.87794991000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tVirginia\t6687651.749161700000000000\t10374539.89684460000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tWashington\t8636272.409946660000000000\t17487417.864883300000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tWest Virginia\t5033892.95645999000000000\t6268945.62724490000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tWisconsin\t7078903.973674470000000000\t16929671.156826900000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: "USA\tWyoming\t2027481.75485764000000000\t25329647.373874300000000000
+  },
+  {
+    iso	adm1	umd_tree_cover_extent_2010__ha	area__ha: ""
+  }
+]
